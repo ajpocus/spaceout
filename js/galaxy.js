24bcb94b9c8c464544a1d33409450bd050b6d0ab
Austin Pocus <ajpocus@gmail.com>
2014-03-04 11:59:41 -0500

    Controls in progress

--- js/galaxy.js
+++ js/galaxy.js
@@ -1,44 +1,47 @@
 define(['three', 'ship', 'ship_controls'], function(three, Ship, ShipControls) {
-    function Galaxy() {
-        var WIDTH = window.innerWidth,
-        HEIGHT = window.innerHeight;
+  function Galaxy() {
+    var WIDTH = window.innerWidth,
+    HEIGHT = window.innerHeight;
+
+    // set some camera attributes
+    var FOV = 90,
+        ASPECT = WIDTH / HEIGHT,
+        NEAR = 0.1,
+        FAR = 10000;
+        
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( FOV, ASPECT, NEAR, FAR );
+    var clock = new THREE.Clock();
     
-        // set some camera attributes
-        var FOV = 90,
-            ASPECT = WIDTH / HEIGHT,
-            NEAR = 0.1,
-            FAR = 10000,
-            ANGLE = 45;
-            
-        var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera( FOV, ASPECT, NEAR, FAR );
-        var controls = new Controls(camera);
-        scene.add(controls);
-        
-        var renderer = new THREE.WebGLRenderer();
-        renderer.setSize( WIDTH, HEIGHT );
-        document.body.appendChild( renderer.domElement );
-        
-        var ship = new Ship(scene);
-        
-        ship.mesh.position.z = -10;
-        camera.lookAt(ship.mesh.position);
-        
-        var pointLight = new THREE.PointLight( 0xffffff, 1, 100 );
-        pointLight.position.set(5, 10, 0);
-        scene.add(pointLight);
-        
-        camera.lookAt(ship.mesh.position);
-        
-        var render = function () {
-    		requestAnimationFrame(render);
-            controls.update();
-    		renderer.render(scene, camera);
-    	};
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( WIDTH, HEIGHT );
+    document.body.appendChild( renderer.domElement );
     
-    	render();
-    }
+    var ship = new Ship(scene);
     
-    return Galaxy;
+    ship.mesh.position.z = -10;
+    camera.lookAt(ship.mesh.position);
     
+    var pointLight = new THREE.PointLight( 0xffffff, 1, 100 );
+    pointLight.position.set(5, 10, 0);
+    scene.add(pointLight);
+    
+    camera.lookAt(ship.mesh.position);
+    
+    var controls = new ShipControls(camera, ship);
+    
+    var render = function () {
+		  requestAnimationFrame(render);
+
+      var delta = clock.getDelta();
+      controls.update(delta);
+
+		  renderer.render(scene, camera);
+	  };
+    
+  	render();
+  }
+  
+  return Galaxy;
+  
 });
